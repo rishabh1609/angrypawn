2750516197fe03889e90a1579c3fb42effb72114
nishant jain <nishantjain91@gmail.com>
2015-02-17 15:26:48 +0530

    design completed subscription remaining

--- js/parseLoad.js
+++ js/parseLoad.js
@@ -8,14 +8,19 @@ function loadBackupScript(callback){
     script.onload = callback;   
     var head = document.getElementsByTagName("head")[0];
     head.appendChild(script);
-}
-
-loadBackupScript(function(){
-    var script = document.createElement('script');
     script.src = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js';
     script.type = 'text/javascript';   
     var head = document.getElementsByTagName("head")[0];
     head.appendChild(script);
+    script.src = './js/ng-infinite-scroll.min.js';
+    script.type = 'text/javascript';   
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(script);
+}
+
+loadBackupScript(function(){
+    var script = document.createElement('script');
+    
     Parse.initialize("qc3ZW7dP2SN3eOzO6fmruWeu4t8IFMCeixHPT63I", "7CqN2NfFitXg6TQWwTkphWZUJrxvlXfiwq6VNrGX");
     console.log("parse initalized");
 });
