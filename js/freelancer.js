eef40ce54b7be5451ca1dd73f782f8f5f535fa67
nishant jain <nishantjain91@gmail.com>
2015-04-03 21:24:01 +0530

    submit form updated

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,3 +1,141 @@
+
+var course; 
+var opens =0;
+function courseurl(courseid) {
+    course=courseid;
+    
+    if(getCookie("email")==""){
+        opens=1;
+        var $anchor = $(this);
+        $('#subscribe').modal('show');
+        alert("Please subscribe with us to get coupon.")
+        
+        return;
+    }
+    var win = window.open("","_blank");
+    
+    Parse.Cloud.run('sendURL', {  courseID: courseid, email:getCookie("email")}, {
+
+        success: function(success) {
+            console.log(success)
+            opens=0;
+            
+            win.location.href=success;
+            win.focus;
+            return
+        },
+        error: function(error) {
+            console.log("err")
+            opens=0;
+            alert("Something went wrong. Try again later.");
+            return;
+        }
+    });
+return;
+}
+
+$('#CourseSubmit').on('click', function(event) {
+    event.preventDefault();
+    console.log("submission started");
+    var iname = $('input[name=instructorName]').val();
+    var iemail = $('input[name=instructorEmail]').val();
+    var curl = $('input[name=courseUrl]').val();
+    var price = parseInt($('input[name=coursePrice]').val());
+    
+     
+    var udemy = curl.search("udemy.com");
+    
+    if(udemy==-1 )
+    {
+        $('#status').text("Course url is incorrect. Please enter a valid url");
+        $('#status').css('color', '#d9534f');
+        return;
+    }
+    console.log(curl);
+    curl = curl.substr(udemy+10);
+    curl = curl.replace(/([^:]\/)\/+/g, "/");
+    var coruseID = curl.substring(0,curl.indexOf("/"));
+                                 
+    var couponCode = curl.search("couponCode=");
+    console.log(couponCode);
+    if(couponCode == -1){
+        $('#status').text("Please enter a url with coupon code");
+        $('#status').css('color', '#d9534f');
+        return;
+    }
+    
+    couponCode= curl.substr(couponCode+11);
+    
+    $('#status').css('color', 'black');
+    $('#status').text("Please wait...submitting your course.");
+
+  
+            Parse.Cloud.run('addCourse', { name: iname, email: iemail, courseID:coruseID,couponCode: couponCode, price:price}, {
+            success: function(success) {
+            console.log("success")
+            $('#status').text("Your course have been submitted successfully");
+            $('#status').css('color', '#5cb85c');
+            },
+            error: function(error) {
+            console.log("err")
+            $('#status').text("Something went wrong. Try again later.");
+             $('#status').css('color', '#d9534f');
+            }
+            });
+    
+    });
+
+
+$('#Subscribebutton').on('click', function(event) {
+    event.preventDefault();
+    console.log("submission started");
+    
+    var iemail = $('input[name=subscribeEmail]').val();
+   
+   
+    
+    Parse.Cloud.run('addSubscriber', {  email: iemail}, {
+        success: function(success) {
+            if(success==0)
+            alert("Submitted Successfully.");
+            else
+            alert("You are already registered :-)")
+            setCookie("email",iemail,7);
+            if(opens==1)
+                courseurl(course);
+        },
+        error: function(error) {
+            console.log("err")
+            if(opens==1)
+                courseurl(course);
+            alert("Something went wrong. Try again later.");
+        }
+    });
+    
+ 
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
+
+
 /*!
  * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
@@ -138,7 +276,7 @@ $(document).ready(function() {
                     notEmpty: {
                         message: 'The course price is required and cannot be empty'
                     },
-                    uri: {
+                    number: {
                         message: 'The input is not a valid number'
                     }
                 }
@@ -156,137 +294,3 @@ $(document).ready(function() {
 
 });
 
-
-var course; 
-var opens =0;
-function courseurl(courseid) {
-    course=courseid;
-    
-    if(getCookie("email")==""){
-        opens=1;
-        var $anchor = $(this);
-        $('#subscribe').modal('show');
-        alert("Please subscribe with us to get coupon.")
-        
-        return;
-    }
-    var win = window.open("","_blank");
-    
-    Parse.Cloud.run('sendURL', {  courseID: courseid, email:getCookie("email")}, {
-
-        success: function(success) {
-            console.log(success)
-            opens=0;
-            
-            win.location.href=success;
-            win.focus;
-            return
-        },
-        error: function(error) {
-            console.log("err")
-            opens=0;
-            alert("Something went wrong. Try again later.");
-            return;
-        }
-    });
-return;
-}
-
-$('#CourseSubmit').on('click', function(event) {
-    event.preventDefault();
-    console.log("submission started");
-    var iname = $('input[name=instructorName]').val();
-    var iemail = $('input[name=instructorEmail]').val();
-    var curl = $('input[name=courseUrl]').val();
-    var price = parseInt($('input[name=coursePrice]').val());
-    
-     
-    var udemy = curl.search("udemy.com");
-    
-    if(udemy==-1 )
-    {
-        $('#status').text("Course url is incorrect. Please enter a valid url");
-        $('#status').css('color', '#d9534f');
-        return;
-    }
-    console.log(curl);
-    curl = curl.substr(udemy+10);
-    curl = curl.replace(/([^:]\/)\/+/g, "/");
-    var coruseID = curl.substring(0,curl.indexOf("/"));
-                                 
-    var couponCode = curl.search("couponCode=");
-    console.log(couponCode);
-    if(couponCode == -1){
-        $('#status').text("Please enter a url with coupon code");
-        $('#status').css('color', '#d9534f');
-        return;
-    }
-    
-    couponCode= curl.substr(couponCode+11);
-    
-    $('#status').css('color', 'black');
-    $('#status').text("Please wait...submitting your course.");
-
-  
-            Parse.Cloud.run('addCourse', { name: iname, email: iemail, courseID:coruseID,couponCode: couponCode, price:price}, {
-            success: function(success) {
-            console.log("success")
-            $('#status').text("Your course have been submitted successfully");
-            $('#status').css('color', '#5cb85c');
-            },
-            error: function(error) {
-            console.log("err")
-            $('#status').text("Something went wrong. Try again later.");
-             $('#status').css('color', '#d9534f');
-            }
-            });
-    
-    });
-
-
-$('#Subscribebutton').on('click', function(event) {
-    event.preventDefault();
-    console.log("submission started");
-    
-    var iemail = $('input[name=subscribeEmail]').val();
-   
-   
-    
-    Parse.Cloud.run('addSubscriber', {  email: iemail}, {
-        success: function(success) {
-            if(success==0)
-            alert("Submitted Successfully.");
-            else
-            alert("You are already registered :-)")
-            setCookie("email",iemail,7);
-            if(opens==1)
-                courseurl(course);
-        },
-        error: function(error) {
-            console.log("err")
-            if(opens==1)
-                courseurl(course);
-            alert("Something went wrong. Try again later.");
-        }
-    });
-    
- 
-});
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
